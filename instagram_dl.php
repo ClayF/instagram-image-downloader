56854725c73e207475e63752b9310ec1960289e0
Clayton <ClayF@users.noreply.github.com>
2016-04-16 15:56:21 -0400

    Added support for concurrent downloads

--- instagram_dl.php
+++ instagram_dl.php
@@ -1,8 +1,10 @@
 <?php
+include("includes/rollingcurlx.class.php");
+
 /**
-*   Crawls through user's page and gets all avaliable images/videos
+*   Crawls through user's page and downloads all avaliable images/videos
 */
-function crawl($username, $items, $max_id) {
+function download($RCX, $username, $max_id = 0) {
     $id = '';
     $lastId = '';
 
@@ -28,10 +30,10 @@ function crawl($username, $items, $max_id) {
     $response = curl_exec($ch);
     curl_close($ch);
     $json = json_decode($response, true);
-
-    // if(empty($json['items'])) {
-    //     die("Invalid username or private.\r\n");
-    // }
+	
+	if(empty($json['items'])) {
+        die("Invalid username, private account or API error.\r\n");
+    }
     	
     // Loop over json, get the filename, URL and timestamp
     foreach ($json['items'] as $data) {
@@ -68,8 +70,8 @@ function crawl($username, $items, $max_id) {
 			}
         }
 
-        // Add file name, url, and upload date to array
-        array_push($items, array($name, $imageURL, $data['created_time']));
+        // Add image to download queue
+		$RCX->addRequest($imageURL, null, 'save', ['fileName' => $name, 'created_time' => $data['created_time'], 'username' => $username]);
 
         // Instagram only shows one page of images at a given time, saves the id of the last image
         $lastId = $data['id'];
@@ -77,28 +79,21 @@ function crawl($username, $items, $max_id) {
 
     // Recurse if more images are avaliable
     if($json['more_available'] == true){
-        return crawl($username, $items, $lastId);
+        return download($RCX, $username, $lastId);
     } else {
-        return $items;
-    }
-}
-
-
-/**
-*   Downloads images returned from crawl()
-*/
-function download($downloadList, $username) {
-    $ch = curl_init();
-    $curl_options = array(
+		$RCX->setOptions([array(
                         CURLOPT_REFERER => "http://instagram.com",
                         CURLOPT_USERAGENT => "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6 (.NET CLR 3.5.30729)",
                         CURLOPT_HEADER => 0,
                         CURLOPT_RETURNTRANSFER => true,
                         CURLOPT_TIMEOUT => 10,
-                    );
-    curl_setopt_array($ch, $curl_options);
+                    )]);
+		$RCX->execute();
+	}
+}
 
-    $saveto = "./" . $username . "/";
+function save($response, $url, $request_info, $user_data, $time) {
+    $saveto = "./" . $user_data['username'] . "/";
 
     // Create user's folder
     if(!file_exists($saveto)) {
@@ -106,38 +101,30 @@ function download($downloadList, $username) {
             die(date("Y-m-d H:i:s") . " - Failed to create folder.\r\n");
         }
     }
-
-    // Download and save image
-    // Download and save image
-    foreach ($downloadList as $data) {
-        $fileName = $data[0];
-        $imageURL = $data[1];
-        $timestamp = $data[2];
-        
-        // Instagram API sometimes gives weird file names
-        if(strpos($fileName, "ig_cache_key")) {
-            $fileName = explode("?", $fileName)[0];
-        }
-        
-        $fileLocation = $saveto . $timestamp . "_" . $fileName;
-        
-        if(!file_exists($fileLocation)) {
-            curl_setopt($ch, CURLOPT_URL, $imageURL);
-            $output = curl_exec($ch);
-
-            // Check error code
-            if(curl_getinfo($ch)['http_code'] == "200") {
-                echo("[" . curl_getinfo($ch)['http_code'] . "] " .date("Y-m-d H:i:s") . " - Downloading " . $fileName . "\r\n");
-            } else {
-                echo("[" . curl_getinfo($ch)['http_code'] . "] " .date("Y-m-d H:i:s") . " - Error downloading " . $fileName . " @ " . $imageURL . "\r\n");
-            }
-
-            $fp = fopen($fileLocation, 'w');
-            fwrite($fp, $output);
-            fclose($fp);
-        }
-    }
-	curl_close($ch);
+	
+	$fileName = $user_data['fileName'];
+	$timestamp = $user_data['created_time'];
+	
+	// Instagram API sometimes gives weird file names
+	if(strpos($fileName, "ig_cache_key")) {
+		$fileName = explode("?", $fileName)[0];
+	}
+	
+	$fileLocation = $saveto . $timestamp . "_" . $fileName;
+	
+	if(!file_exists($fileLocation)) {
+		// Check error code
+		if($request_info['http_code'] == "200") {
+			echo("[" . $request_info['http_code'] . "] " .date("Y-m-d H:i:s") . " - saved " . $fileName . "\r\n");
+		} else {
+			echo("[" . $request_info['http_code'] . "] " .date("Y-m-d H:i:s") . " - Error downloading " . $fileName . " @ " . $url . "\r\n");
+			return;
+		}
+		
+		$fp = fopen($fileLocation, 'w');
+		fwrite($fp, $response);
+		fclose($fp);
+	}
 }
 
 if(!isset($argv[1]) || empty($argv[1])) {
@@ -148,5 +135,5 @@ if (!function_exists('curl_init')) {
     die(date("Y-m-d H:i:s") . " - cURL is not installed.\r\n");
 }
 
-download(crawl($argv[1], array(), 0), $argv[1]);
+download(new RollingCurlX(10), $argv[1], 0);
 ?>
